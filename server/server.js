01fec137701d2f0cd31a55745f407c14bdf2b665
SandeepaniGunasekara <115693602+SandeepaniGunasekara@users.noreply.github.com>
2023-06-30 01:47:53 +0530

    init IntelliJ and created routes

--- server/server.js
+++ server/server.js
@@ -2,13 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+const routes=require('./routes/cv-routes');
 
 // defining the Express app
 const app = express();
-// defining an array to work as the database (temporary solution)
-const ads = [
-  {title: 'Hello, world (again)!'}
-];
+
 
 // using bodyParser to parse JSON bodies into JS objects
 app.use(bodyParser.json());
@@ -19,10 +17,8 @@ app.use(cors());
 // adding morgan to log HTTP requests
 app.use(morgan('combined'));
 
-// defining an endpoint to return all ads
-app.get('/', (req, res) => {
-  res.send(ads);
-});
+//mounting routes
+app.use(routes)
 
 // starting the server
 app.listen(3001, () => {
